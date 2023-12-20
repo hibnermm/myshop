df609fbd4e9553dfd6e5d6465cf4c87b44ea93d6
Melissa <hibnermm@gmail.com>
2023-12-19 23:10:28 -0800

    add product data and mapping, rating, and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 import {Container} from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
+    <Router>
       <Header/>
-
       <main>
-      <Container> <HomeScreen/></Container>
+      <Container> 
+        <Routes>
+          <Route path="/" exact element={<HomeScreen/>} />
+          <Route path="/product/:id" element={<ProductScreen/>} />
+        </Routes>
+        </Container>
       </main>
-
       <Footer/>
+      </Router>
     </>
   );
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import products from '../products'
+import Product from '../components/Product'
+import {Col, Row} from 'react-bootstrap'
 
 const HomeScreen = () => {
   return (
-    <div>Other</div>
+    <>
+    <h1> Latest Products </h1>
+    <Row>
+      {products.map(p => (     //add in { }if more than 1 line code
+        <Col sm={12} md={6} lg={4} xl={3}><Product product={p}/></Col>
+      ))
+      }
+    </Row>
+    </>
   )
 }
 
